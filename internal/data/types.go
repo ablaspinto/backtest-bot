7e6bb0889e1c8513befe3013bc81dc233c5ccf1d
ablaspinto <ablaspinto@gmail.com>
2025-12-30 19:42:05 -0500

    added Bar type, will add more probably once, needed

---
+++ internal/data/types.go
@@ -0,0 +1,11 @@
+package data
+
+type Bar struct {
+	Date         string
+	Open         float32
+	High         float32
+	Low          float32
+	Close        float32
+	Volume       float32
+	OpenInterest float32
+}
