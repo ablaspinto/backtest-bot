4f11195659ed87a16453809c2aa64829550d022f
ablaspinto <ablaspinto@gmail.com>
2025-12-30 19:41:19 -0500

    Load single file, works, will need to implement new function, to recursively parse other files

---
+++ internal/data/load.go
@@ -0,0 +1,62 @@
+package data
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Loader interface {
+	LoadSingleFile(filename string) ([]Bar, error)
+}
+
+type ldr struct{}
+
+func NewLoader() Loader {
+	return &ldr{}
+
+}
+
+func (l *ldr) LoadSingleFile(filename string) ([]Bar, error) {
+	p := &errParser{}
+	var bars []Bar
+	path := filepath.Join("internal", "historial_data", "archive", filename)
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Print("Error opening File\n")
+		return []Bar{}, ErrLoadingFile
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Print("Error with records\n")
+		return []Bar{}, ErrWithRecords
+	}
+
+	for i, record := range records {
+		if i == 0 {
+			continue
+		}
+		date := record[0]
+		open := p.parseFloat(record[1])
+		high := p.parseFloat(record[2])
+		low := p.parseFloat(record[3])
+		closeVal := p.parseFloat(record[4])
+		volume := p.parseFloat(record[5])
+		openInterest := p.parseFloat(record[6])
+		newBar := Bar{
+			Date:         date,
+			Open:         open,
+			High:         high,
+			Low:          low,
+			Close:        closeVal,
+			Volume:       volume,
+			OpenInterest: openInterest,
+		}
+		bars = append(bars, newBar)
+	}
+	return bars, nil
+
+}
