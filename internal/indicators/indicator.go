e2d13734cc6884b5eef2376a1068b223c5d25893
ablaspinto <ablaspinto@gmail.com>
2026-01-02 22:22:34 -0500

    added EMA formual, and completed implmenetation, will work on learning RSI next

--- internal/indicators/indicator.go
+++ internal/indicators/indicator.go
@@ -2,6 +2,7 @@ package indicators
 
 import (
 	"cmd/internal/data"
+	"fmt"
 )
 
 func SMA(bars []data.Bar, period int) []float32 {
@@ -22,4 +23,26 @@ func SMA(bars []data.Bar, period int) []float32 {
 		}
 	}
 	return smaAverages
+}
+
+func EMA(bars []data.Bar, period float32, firstSmaValue float32) []float32 {
+	var emaAverages []float32
+	// EMA = (priceToday * k) + (emaYesterday * (1 - k))
+	// k = 2 / (period + 1)
+	k := float32(2) / (period + float32(1))
+	j := 0
+	fmt.Printf("K: %v\n", k)
+	for i := 11; i < len(bars)-1; i++ { // start at 10, since we averaged out for the first sMA value
+		closePrice := bars[i].Close
+		if i == 11 {
+			ema := (closePrice * k) + (firstSmaValue * (1 - k))
+			emaAverages = append(emaAverages, ema)
+			continue
+		}
+		ema := (closePrice * k) + (emaAverages[j] * (1 - k))
+		emaAverages = append(emaAverages, ema)
+		j++
+	}
+
+	return emaAverages
 }
