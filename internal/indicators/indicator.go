e598788688b39e6c7b6a55168bc563928e18e8fc
ablaspinto <ablaspinto@gmail.com>
2026-01-03 14:45:39 -0500

    added RSI function, naive implementation completed for initial testing

--- internal/indicators/indicator.go
+++ internal/indicators/indicator.go
@@ -3,6 +3,7 @@ package indicators
 import (
 	"cmd/internal/data"
 	"fmt"
+	"math"
 )
 
 func SMA(bars []data.Bar, period int) []float32 {
@@ -45,4 +46,57 @@ func EMA(bars []data.Bar, period float32, firstSmaValue float32) []float32 {
 	}
 
 	return emaAverages
+}
+
+func RSI(bars []data.Bar, period float32) []float32 {
+	var rsiValues []float32
+	var gains []float32
+	var losses []float32
+
+	for i := range len(bars) {
+		currPrice := bars[i].Close
+		if i == 0 {
+			continue
+		}
+		prevPrice := bars[i-1].Close
+		difference := currPrice - prevPrice
+		if difference > 0 {
+			gains = append(gains, difference)
+			losses = append(losses, 0)
+		} else if difference < 0 {
+			absVal := math.Abs(float64(difference))
+			losses = append(losses, float32(absVal))
+			gains = append(gains, 0)
+		} else {
+			gains = append(gains, 0)
+			losses = append(losses, 0)
+		}
+	}
+	simpleGainAvg := float32(0)
+	simpleLossAvg := float32(0)
+	for i := range int32(period) {
+		simpleGainAvg += gains[i]
+		simpleLossAvg += losses[i]
+	}
+	simpleGainAvg = simpleGainAvg / float32(period)
+	simpleLossAvg = simpleLossAvg / float32(period)
+
+	RS := simpleGainAvg / simpleLossAvg
+	firstRSI := 100 - (100 / (1 + RS))
+	rsiValues = append(rsiValues, firstRSI)
+
+	for i := int(period); i < len(gains); i++ {
+		currGain := gains[i]
+		currLoss := losses[i]
+		simpleGainAvg = ((simpleGainAvg)*(period-1) + currGain) / period
+		simpleLossAvg = ((simpleLossAvg)*(period-1) + currLoss) / period
+		if simpleLossAvg == 0 {
+			rsiValues = append(rsiValues, 100)
+			continue
+		}
+		secondRs := simpleGainAvg / simpleLossAvg
+		secRSI := 100 - (100 / (1 + secondRs))
+		rsiValues = append(rsiValues, secRSI)
+	}
+	return rsiValues
 }
