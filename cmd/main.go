61cc55f44bd924629dcbdf10618518ef4f219cfc
ablaspinto <ablaspinto@gmail.com>
2026-01-04 16:11:43 -0500

    added MACD to main, historgram

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,18 @@ import (
 
 func main() {
 	loader := data.NewLoader()
-	bars, err := loader.LoadSingleFile("CME_ESH2000.csv")
+	bars, err := loader.LoadSingleFile("CME_ESH2001.csv")
 	if err != nil {
 		fmt.Printf("error loading bars")
 	}
 	smaObj := indicators.SMA(bars, 10)
 	fmt.Printf("SMA BARS: %v\n", smaObj)
-	emaObj := indicators.EMA(bars, 10, smaObj[0])
+	emaObj := indicators.EMA(bars, 10)
 	fmt.Printf("EMA BARS: %v\n", emaObj)
 	rsiObj := indicators.RSI(bars, 14)
 	fmt.Printf("RSI : %v\n", rsiObj)
+
+	macd := indicators.MACD(bars)
+	fmt.Printf("MACD HISTOGRAM: %v\n", macd)
 
 }
