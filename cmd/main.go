68883b138094cdfcdce81193ba626c2f73b24bb5
ablaspinto <ablaspinto@gmail.com>
2026-01-06 21:40:47 -0500

    upadted main function,to reused postgres db

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,51 @@
 package main
 
 import (
-	"cmd/internal/data"
-	"cmd/internal/indicators"
-	"fmt"
+	"cmd/internal/env"
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	loader := data.NewLoader()
-	bars, err := loader.LoadSingleFile("CME_ESH2001.csv")
-	if err != nil {
-		fmt.Printf("error loading bars")
+	//loader := data.NewLoader()
+	//bars, err := loader.LoadSingleFile("CME_ESH2001.csv")
+	//smaObj := indicators.SMA(bars, 10)
+	//fmt.Printf("SMA BARS: %v\n", smaObj)
+	//emaObj := indicators.EMA(bars, 10)
+	//fmt.Printf("EMA BARS: %v\n", emaObj)
+	//rsiObj := indicators.RSI(bars, 14)
+	//fmt.Printf("RSI : %v\n", rsiObj)
+	//macd := indicators.MACD(bars)
+	//fmt.Printf("MACD HISTOGRAM: %v\n", macd)
+	ctx := context.Background()
+	cfg := config{
+		addr: ":8080",
+		dbConfig: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=marketsim sslmode=disable"),
+		},
 	}
-	smaObj := indicators.SMA(bars, 10)
-	fmt.Printf("SMA BARS: %v\n", smaObj)
-	emaObj := indicators.EMA(bars, 10)
-	fmt.Printf("EMA BARS: %v\n", emaObj)
-	rsiObj := indicators.RSI(bars, 14)
-	fmt.Printf("RSI : %v\n", rsiObj)
+	// logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 
-	macd := indicators.MACD(bars)
-	fmt.Printf("MACD HISTOGRAM: %v\n", macd)
+	// database
+	conn, err := pgx.Connect(ctx, cfg.dbConfig.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
 
+	logger.Info("Connected to database", "dsn", cfg.dbConfig.dsn)
+	api := application{
+		config: cfg,
+		db:     conn,
+	}
+
+	if err := api.run(api.mount()); err != nil {
+		logger.Info("server has failed to start %v\n", err)
+		os.Exit(1)
+	}
 }
