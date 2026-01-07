43594de38863f8d8c831b950cbbeb21d252a3d46
ablaspinto <ablaspinto@gmail.com>
2026-01-07 15:38:38 -0500

    added id converter for hwen looking up things by id

---
+++ internal/response_handlers/int_converter.go
@@ -0,0 +1,21 @@
+package reponsehandlers
+
+import (
+	"cmd/internal/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func ParseIdResponse(r *http.Request, w http.ResponseWriter, fieldName string) (int64, bool) {
+	strId := chi.URLParam(r, fieldName)
+	num, err := strconv.ParseInt(strId, 10, 64)
+	if err != nil {
+		log.Printf("failed to parse %s: Error: %v\n", fieldName, err)
+		json.WriteError(w, http.StatusBadRequest, "Invalid Request")
+		return 0, false
+	}
+	return int32(num), true
+}
