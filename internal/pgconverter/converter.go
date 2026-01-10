45d87d1c5ac0cd1bb68a305e0b3af83c3f3b5a34
ablaspinto <ablaspinto@gmail.com>
2026-01-09 20:30:12 -0500

    added conversions from pg types to go literals and other way

--- internal/pgconverter/converter.go
+++ internal/pgconverter/converter.go
@@ -2,6 +2,7 @@ package pgconverter
 
 import (
 	"math/big"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgtype"
 )
@@ -20,5 +21,30 @@ func PGNumericConverter(i string) pgtype.Numeric {
 	bigIntVal := new(big.Int)
 	bigIntVal.SetString(i, 10)
 	return pgtype.Numeric{Valid: true, Int: bigIntVal}
+}
+
+func PGInt4Converter(i int32) pgtype.Int4 {
+	return pgtype.Int4{Int32: i, Valid: true}
+
+}
+
+func PGTimeStampConverter(s string) pgtype.Timestamptz {
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return pgtype.Timestamptz{Valid: false}
+	}
+	ts := pgtype.Timestamptz{
+		Time:  t,
+		Valid: true,
+	}
+	return ts
+}
+
+func PGToBool(s pgtype.Bool) bool {
+	return s.Bool
+}
+
+func Int4ToInt32(num pgtype.Int4) int32 {
+	return num.Int32
 
 }
