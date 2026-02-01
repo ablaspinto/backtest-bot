fdec18a2005e5dabcda78eb091338790d0057676
ablaspinto <ablaspinto@gmail.com>
2026-01-31 19:04:47 -0500

    removed htmx

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"cmd/internal/env"
 	"context"
-	"html/template"
 	"log/slog"
 	"os"
 
@@ -11,45 +10,42 @@ import (
 )
 
 func main() {
+	//loader := data.NewLoader()
+	//bars, err := loader.LoadSingleFile("CME_ESH2001.csv")
+	//smaObj := indicators.SMA(bars, 10)
+	//fmt.Printf("SMA BARS: %v\n", smaObj)
+	//emaObj := indicators.EMA(bars, 10)
+	//fmt.Printf("EMA BARS: %v\n", emaObj)
+	//rsiObj := indicators.RSI(bars, 14)
+	//fmt.Printf("RSI : %v\n", rsiObj)
+	//macd := indicators.MACD(bars)
+	//fmt.Printf("MACD HISTOGRAM: %v\n", macd)
 	ctx := context.Background()
-
-	// Logger
+	cfg := config{
+		addr: ":8080",
+		dbConfig: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=marketsim sslmode=disable"),
+		},
+	}
+	// logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	// Templates
-	templates, err := template.ParseGlob("web/templates/layouts/*.html")
+	// database
+	conn, err := pgx.Connect(ctx, cfg.dbConfig.dsn)
 	if err != nil {
-		logger.Error("failed to parse layout templates", "error", err)
-		os.Exit(1)
-	}
-	templates, err = templates.ParseGlob("web/templates/pages/*.html")
-	if err != nil {
-		logger.Error("failed to parse page templates", "error", err)
-		os.Exit(1)
-	}
-
-	// Database
-	dsn := env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=marketsim sslmode=disable")
-	conn, err := pgx.Connect(ctx, dsn)
-	if err != nil {
-		logger.Error("failed to connect to database", "error", err)
-		os.Exit(1)
+		panic(err)
 	}
 	defer conn.Close(ctx)
-	logger.Info("Connected to database")
 
-	app := &application{
-		config: config{
-			addr:     ":8080",
-			dbConfig: dbConfig{dsn: dsn},
-		},
-		db:        conn,
-		templates: templates,
+	logger.Info("Connected to database", "dsn", cfg.dbConfig.dsn)
+	api := application{
+		config: cfg,
+		db:     conn,
 	}
 
-	if err := app.run(app.mount()); err != nil {
-		logger.Error("server failed", "error", err)
+	if err := api.run(api.mount()); err != nil {
+		logger.Info("server has failed to start %v\n", err)
 		os.Exit(1)
 	}
 }
